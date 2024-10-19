c91023e9eabe6f2811dc8089c0a620c072c3a348
kevin <kevinr060799@gmaail.com>
2024-10-19 15:33:13 -0500

    se cambia la url del backend

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   
   const handleLogin = async (username, password) => {
     try {
-      const response = await fetch('http://localhost:5000/api/login', { 
+      const response = await fetch('https://web-back-nu.vercel.app/api/login', { 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),

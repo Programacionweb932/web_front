eb7946f9b00c821ce7faf9a96cf7e94e46491346
kevin <kevinr060799@gmaail.com>
2024-11-14 21:18:25 -0500

    se agrega el registro del administrador

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   const handleLogin = async (username, password) => {
     try {
-      const response = await fetch('https://web-back-eight.vercel.app/api', { 
+      const response = await fetch('https://web-back-eight.vercel.app/api/login', { 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
@@ -49,7 +49,7 @@ function App() {
 
   const handleNavigateToLogin = () => {
     setView('login');
-    setView('admin')
+    setView('admin');
     navigate('/login'); 
     navigate('/admin');
   };

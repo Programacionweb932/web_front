d727816224fb214338bb9e2c3ae00737b2399e94
kevin <kevinr060799@gmail.com>
2024-11-18 15:27:38 -0500

    Se modifica las imagenes del blog

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Define la carpeta de salida para la construcción del proyecto
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Agrega un alias para importar desde src
-    },
-  },
-  server: {
-    port: 3000, // Configura el puerto para el desarrollo local
-  },
-});
+})
